850e21ff71a37c187d0e443617f599334c396277
SzCsaba01 <szikszaicsaba@yahoo.com>
2025-06-24 22:35:43 +0300

    feat(layout): Added private page layouts with sidebar and navbar

--- FitTrack-Frontend/src/app/layouts/main-layout/main-layout.ts
+++ FitTrack-Frontend/src/app/layouts/main-layout/main-layout.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, OnInit, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MOBILE_BRAKEPOINT } from '../../constants/sizes.constant';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatIconModule, RouterModule],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
-export class MainLayout {}
+export class MainLayout implements OnInit {
+  private readonly screenWidth = signal(window.innerWidth);
+
+  readonly isMobile = computed(() => this.screenWidth() < MOBILE_BRAKEPOINT);
+  readonly isSidebarOpen = signal(this.screenWidth() >= MOBILE_BRAKEPOINT);
+
+  readonly isCollapsed = computed(
+    () => !this.isMobile() && !this.isSidebarOpen(),
+  );
+
+  ngOnInit(): void {
+    window.addEventListener('resize', () => {
+      const width = window.innerWidth;
+      this.screenWidth.set(width);
+      if (width >= MOBILE_BRAKEPOINT) {
+        this.isSidebarOpen.set(true);
+      } else {
+        this.isSidebarOpen.set(false);
+      }
+    });
+  }
+
+  toggleSidebar(): void {
+    this.isSidebarOpen.update((open) => !open);
+  }
+
+  closeSidebar(): void {
+    this.isSidebarOpen.set(false);
+  }
+}
